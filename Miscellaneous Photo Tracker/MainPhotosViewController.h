3432dea740092e284e06aa48c56e62fd922d0cee
Ryan D'souza <dsouzarc@gmail.com>
2015-07-07 23:23:35 -0400

    Primitive views

---
+++ Miscellaneous Photo Tracker/MainPhotosViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainPhotosViewController.h
+//  Miscellaneous Photo Tracker
+//
+//  Created by Ryan D'souza on 7/7/15.
+//  Copyright (c) 2015 Ryan D'souza. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface MainPhotosViewController : ViewController
+
+@end
---
+++ Miscellaneous Photo Tracker/MainPhotosViewController.m
@@ -0,0 +1,43 @@
+//
+//  MainPhotosViewController.m
+//  Miscellaneous Photo Tracker
+//
+//  Created by Ryan D'souza on 7/7/15.
+//  Copyright (c) 2015 Ryan D'souza. All rights reserved.
+//
+
+#import "MainPhotosViewController.h"
+
+@interface MainPhotosViewController ()
+
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
+
+@end
+
+@implementation MainPhotosViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+
+- (IBAction)editPhotosButton:(id)sender {
+    
+}
+
+- (IBAction)goToOldPhotosButton:(id)sender {
+    
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
